82266ad3ce942f052a5411fc949e5fccbc73c75f
unknown <sujoydaspc2023@gmail.com>
2025-03-01 23:55:27 +0600

    adding google signin

--- src/Components/AuthProvider/AuthProvider.jsx
+++ src/Components/AuthProvider/AuthProvider.jsx
@@ -1,15 +1,34 @@
 import { createContext, useEffect, useState } from "react"
-
+import { , GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import auth from "../Firebase/Firebase.config";
 export const AuthContext = createContext(null)
 
 // eslint-disable-next-line react/prop-types
 const AuthProvider = ({ children }) => {
     const [products, setProducts] = useState([])
+    const [user, setUser] = useState(null);
+
     useEffect(() => {
         fetch('/public/product.json')
             .then(res => res.json())
             .then(data => setProducts(data))
     }, [])
+
+    const googleProvider = new GoogleAuthProvider();
+
+    const signInWithGoogle = () => {
+        // setLoading(true);
+        return signInWithPopup(auth, googleProvider)
+    }
+
+
+
+
+
+
+
+
+
 
     const dataContext = {
         products,
